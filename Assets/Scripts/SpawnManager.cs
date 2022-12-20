9eb2d199cb13e8fe63007aeafb26bc543191fc8f
2Karl <gosub1250@gmail.com>
2022-12-20 16:49:52 +0000

    Spawnmanager
    Updated spawnmanager
    
    TODO: object serialisation.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,14 +9,65 @@ public class SpawnManager : MonoBehaviour
     private enum EnemyType {slow, medium, fast};
     private float xRange = 20;
     private float zRange = 15;
+    private List<EnemyWave> enemyWaves = new List<EnemyWave>();
+    [SerializeField] private bool readyToSpawn = false;
+    [SerializeField] private int currentWave = 0;
+    [SerializeField] private int currentSubwave = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        // This should read in values froma  JSON file and spawn in waves based on that
-        SpawnEnemies(EnemyType.slow, 2);
-        SpawnEnemies(EnemyType.medium, 4);
-        SpawnEnemies(EnemyType.fast, 3);
+        // This is messy. Fix it.
+        enemyWaves.Add(new EnemyWave());
+        enemyWaves[0].subwaves.Add(new int[] {2,0,0,10});
+        enemyWaves[0].subwaves.Add(new int[] {0,4,0,10});
+        enemyWaves[0].subwaves.Add(new int[] {1,0,1,0});
+        enemyWaves.Add(new EnemyWave());
+        enemyWaves[1].subwaves.Add(new int[] {2,0,0,0});
+        StartCoroutine(WaitForNextSpawn(10));
+    }
+
+    void Update()
+    {
+        if (readyToSpawn)
+        {
+            readyToSpawn = false;
+            SpawnNextWave();
+        }
+    }
+
+    void SpawnNextWave()
+    {
+        if (currentWave == enemyWaves.Count){
+            // In theory you've won the game at this point
+            return;
+        }
+        
+        // spawn slow, medium and fast enemies according to subwave info
+        SpawnEnemies(EnemyType.slow, enemyWaves[currentWave].subwaves[currentSubwave][0]);
+        SpawnEnemies(EnemyType.medium, enemyWaves[currentWave].subwaves[currentSubwave][1]);
+        SpawnEnemies(EnemyType.fast, enemyWaves[currentWave].subwaves[currentSubwave][2]);
+        if (enemyWaves[currentWave].subwaves[currentSubwave][3] > 0){
+            StartCoroutine(WaitForNextSpawn(enemyWaves[currentWave].subwaves[currentSubwave][3]));
+        }
+        else {
+            StartCoroutine(WaitForNextSpawn(30));
+        }
+        currentSubwave++;   
+        if (currentSubwave == enemyWaves[currentWave].subwaves.Count){
+            currentWave ++;
+            currentSubwave = 0;
+        }
+        // start waitfornextspawn coroutine if time is non-zero
+        // if the time to next is 0, advance to next wave
+        
+    }
+
+    IEnumerator WaitForNextSpawn(int delay)
+    {
+        yield return new WaitForSeconds(delay);
+        readyToSpawn = true;
+        
     }
 
     // Not sure about this. Worth refactoring when I implement the JSON but I need to plan it out a bit.
@@ -48,5 +99,12 @@ public class SpawnManager : MonoBehaviour
         
             Instantiate(enemyPrefabs[(int)enemyType], spawnPos, enemyPrefabs[(int)enemyType].transform.rotation);
         }
+    }
+
+    [System.Serializable]
+    class EnemyWave
+    {
+        public List<int[]> subwaves = new List<int[]>();
+        
     }
 }
